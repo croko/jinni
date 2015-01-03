1e51e3e520d640c35f47e38668adb74df25a3b1b
Gennady Kudelya <gennady.kudelya@gmail.com>
2015-01-03 21:01:20 +0200

    workaround ninefold migration error

--- db/migrate/20150103085505_add_url_to_foundation.rb
+++ db/migrate/20150103085505_add_url_to_foundation.rb
@@ -1,5 +1,9 @@
 class AddUrlToFoundation < ActiveRecord::Migration
   def change
-    remove_column :foundations, :website_url, :string
+    add_column :foundations, :logo, :string
+    add_column :foundations, :website_url, :string
+    add_column :foundations, :twitter_url, :string
+    add_column :foundations, :facebook_url, :string
+    add_column :foundations, :google_url, :string
   end
 end
