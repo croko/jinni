43152cd751a8b7caf1888a87c7773f7e40fcfe44
Gennady Kudelya <gennady.kudelya@gmail.com>
2015-01-08 18:22:17 +0200

    temp fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,19 +7,19 @@ class PagesController < ApplicationController
     @finished_projects = Project.close.approved.published.limit(8).order("RANDOM()")
     @foundations = Foundation.published.limit(3).order("RANDOM()")
 
-    project = @projects.first
-    set_meta_tags og: {
-                      title: project.title,
-                      description: project.goal,
-                      url: root_url,
-                      image: project.image_full_size
-                  }
-
-    set_meta_tags twitter: {
-                      card: "summary_large_image",
-                      site: root_url,
-                      title: project.title,
-                      image: {src: project.image_full_size}
-                  }
+    # project = @projects.first
+    # set_meta_tags og: {
+    #                   title: project.title,
+    #                   description: project.goal,
+    #                   url: root_url,
+    #                   image: project.image_full_size
+    #               }
+    #
+    # set_meta_tags twitter: {
+    #                   card: "summary_large_image",
+    #                   site: root_url,
+    #                   title: project.title,
+    #                   image: {src: project.image_full_size}
+    #               }
   end
 end
