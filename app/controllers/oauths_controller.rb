5d0d34ce61a426264205855743e4d12bcfba184d
Gennady Kudelya <gennady.kudelya@gmail.com>
2015-01-09 20:34:21 +0200

    refactoring in oauth controller

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -18,32 +18,34 @@ class OauthsController < ApplicationController
           reset_session # protect from session fixation attack
           auto_login(@user)
           add_provider_to_user(provider)
+          fb_posts(provider)
         else
           @user = create_from(provider)
           # NOTE: this is the place to add '@user.activate!' if you are using user_activation submodule
           reset_session # protect from session fixation attack
           auto_login(@user)
-
-          # for FB posts
-          if provider == 'facebook'
-            auth_fb = @user.authentications.find_by(provider: 'facebook')
-            auth_fb.update_column('access_token', @access_token.token.to_s)
-          end
-
+          fb_posts(provider)
           if provider == 'google'
             auth = @user.authentications.find_by(provider: 'google')
             auth.update_attribute('avatar_url', user_hash[:user_info]['picture']) if auth
           end
-
           redirect_to edit_user_path(@user), notice: "#{t 'session.registration_success'} #{provider.titleize}!" and return
         end
-
         redirect_to projects_path, :notice => "#{t 'session.login_success'} #{provider.titleize}!"
       end
     rescue
       redirect_to login_url, alert: "#{t 'session.login_fail'} #{provider.titleize}!"
     end
   end
+
+  def fb_posts(provider)
+    # for FB posts
+    if provider == 'facebook'
+      auth_fb = @user.authentications.find_by(provider: 'facebook')
+      auth_fb.update_column('access_token', @access_token.token.to_s)
+    end
+  end
+
 
   private
   def auth_params
