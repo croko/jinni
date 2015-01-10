df70a4cc75b09a07aacbc5db9c550e8a24b99f7f
Gennady Kudelya <gennady.kudelya@gmail.com>
2015-01-10 10:51:41 +0200

    configured smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,13 +67,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'jinni.com.ua' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
     user_name:            'jinni.info@gmail.com',
     password:             'GfhjkmRhjrj115',
-    domain: 'gmail.com',
+    domain: 'jinni.com.ua',
     authentication:       'plain',
     enable_starttls_auto: true  }
 
