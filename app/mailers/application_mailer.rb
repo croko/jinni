e35ce5997821d0c401a847ea49781338f631dd05
Gennady Kudelya <gennady.kudelya@gmail.com>
2015-01-10 10:37:36 +0200

    fixed typo in email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "jinni.info@gmail.com",
           to: 'inna.kudelya@gmail.com',
-          bcc: 'gennady.kidelya@gmail.com'
+          bcc: 'gennady.kudelya@gmail.com'
 
   # layout 'mailer'
 end
