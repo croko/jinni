4f19831c091ac192b2ae2e10a15ed59be75ecb78
Gennady Kudelya <gennady.kudelya@gmail.com>
2015-01-10 10:16:20 +0200

    configured smtp

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "jinni.info@gmail.com"
   default to: 'inna.kudelya@gmail.com'
-  default bcc: 'gennady.kidelya@gmail.com'
 
   # layout 'mailer'
 end
--- app/mailers/system_mailer.rb
+++ app/mailers/system_mailer.rb
@@ -2,24 +2,27 @@ class SystemMailer < ApplicationMailer
 
   def new_user_registered(user)
     @user = user
+    @bcc = 'gennady.kidelya@gmail.com'
 
-    mail(subject: "Зарегистрировался #{@user.fio}")  do |format|
+    mail(bcc: @bcc, subject: "Зарегистрировался #{@user.fio}")  do |format|
       format.html
     end
   end
 
   def new_project_registered(project)
     @project = project
+    @bcc = 'gennady.kidelya@gmail.com'
 
-    mail(subject: "Новый проект #{@project.title}")  do |format|
+    mail(bcc: @bcc, subject: "Новый проект #{@project.title}")  do |format|
       format.html
     end
   end
 
   def new_project_published(project)
     @project = project
+    @bcc = 'gennady.kidelya@gmail.com'
 
-    mail(subject: "Опубликован новый проект #{@project.title}")  do |format|
+    mail(bcc: @bcc, subject: "Опубликован новый проект #{@project.title}")  do |format|
       format.html
     end
   end
