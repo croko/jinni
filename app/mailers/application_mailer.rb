7b24a0cda34d8f9ebff40846ff274a822ad10675
Gennady Kudelya <gennady.kudelya@gmail.com>
2015-01-10 10:28:20 +0200

    configured smtp

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "jinni.info@gmail.com"
-  default to: 'inna.kudelya@gmail.com'
+  default from: "jinni.info@gmail.com",
+          to: 'inna.kudelya@gmail.com',
+          bcc: 'gennady.kidelya@gmail.com'
 
   # layout 'mailer'
 end
