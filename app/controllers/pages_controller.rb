cc0f1b4eaffd9520e762a8de9c27bf4706232236
Gennady Kudelya <gennady.kudelya@gmail.com>
2015-01-18 18:16:16 +0200

    featured_projects and projects moved to one section in pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :require_login
 
   def index
-    @featured_projects = Project.opened.approved.featured.published.limit(3).order("RANDOM()")
-    @projects = Project.opened.exclude_featured.approved.published.limit(3).order("RANDOM()")
+    featured_projects = Project.opened.featured.approved.published.limit(6).order("RANDOM()")
+    @projects = featured_projects + Project.opened.exclude_featured.approved.published.limit(6).order("RANDOM()")
     @finished_projects = Project.close.approved.published.limit(8).order("RANDOM()")
     @foundations = Foundation.published.limit(3).order("RANDOM()")
   end
