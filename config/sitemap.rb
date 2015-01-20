ade9c85142e0d151c11794bb09571f325c4318c6
Gennady Kudelya <gennady.kudelya@gmail.com>
2015-01-20 12:05:21 +0200

    changed projects in sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -4,7 +4,7 @@ sitemap :site do
   url root_url, last_mod: Time.zone.now, change_freq: "daily", priority: 1.0
   url projects_url
 
-  Project.featured.approved.published.each do |project|
+  Project.approved.published.each do |project|
     url project, last_mod: project.updated_at, priority: (project.featured ? 1.0 : 0.7)
   end
 end
