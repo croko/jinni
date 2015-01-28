79d9ffee4a6bbb3429da7b927197d66ea8aa26af
Gennady Kudelya <gennady.kudelya@gmail.com>
2015-01-28 17:22:47 +0200

    added wysihtml5 to assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
- Rails.application.config.assets.precompile += %w(bootstrap-wysihtml5/locales/ru-RU.js)
+ Rails.application.config.assets.precompile += %w(bootstrap-wysihtml5/index.js bootstrap-wysihtml5/locales/ru-RU.js)
